116f3f6b61a2422fd2f8c2c6c958aa64191ef218
philip <Pu23667@essex.ac.uk>
2026-09-03 21:02:27 +0100

    My second commit

---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main () {
+	println!(); // prints an empty line
+
+	println!("hello"); //prints hello
+
+	println!("format {} arguments","some"); //prints format some arguments
+
+	println!("My name is {}. I am a billioniare","Uzomba Nina");
+} 
+
